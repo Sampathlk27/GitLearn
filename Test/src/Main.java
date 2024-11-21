f8d3559c7bf89d820b5470aa70c15ef3bc69296e
Sampath L K <70970268+Sampathlk27@users.noreply.github.com>
2024-11-21 21:31:41 +0530

    This is a mew push

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,13 +1,43 @@
+import java.util.Scanner;
+
+class School
+{
+    Scanner sc=new Scanner(System.in);
+
+    public int id;
+    public String name;
+    public int age;
+    public int marks;
+    String grade="fail";
+    public School()
+    {
+        marks=0;
+        id=0;
+    }
+    public void person()
+    {
+        System.out.println("Enter id:");
+        id=new Scanner(System.in).nextInt();
+        System.out.println("Enter name:");
+        name=sc.next();
+        System.out.println("Enter age:");
+        age=new Scanner(System.in).nextInt();
+        System.out.println("Enter marks:");
+        marks= sc.nextInt();
+        if (marks>50) {
+            grade = "Pass";
+        }
+        System.out.println("Id" + id + "Name" + name + " Age" + age +" Mark "+ marks + grade);
+    }
+}
+
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
-        System.out.println("sfsdfdsfds");
-        System.out.println("Hello, World!"); System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
 
 
+        School s=new School();
+        s.person();
     }
 }
